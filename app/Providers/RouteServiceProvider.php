24cac1f5ab95fd4c5a2aa39a6847c66b12782aff
Salman Amin <chand334@gmail.com>
2019-05-03 11:23:54 +0500

    modified homepage

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,6 +27,9 @@ class RouteServiceProvider extends ServiceProvider
     {
         //
         Route::bind('slug', function($slug){
+            //return Question::with(['answers.user', 'answers' => function($query){
+              //  $query->orderBy('votes_count', 'DESC');
+            //}])->where('slug', $slug)->first() ?? abort(404);
             return Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
             //return $question ? $question : abort(404);
         });
